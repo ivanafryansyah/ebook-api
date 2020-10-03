24b2f2f2b9fa860001772b4cc44169a0682d233f
ivanafransyah <ivanafransyah0@gmail.com>
2020-10-03 23:38:13 +0700

    Add auth

--- config/app.php
+++ config/app.php
@@ -174,6 +174,15 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // /*
+        // * Package Service Providers...
+        // */
+
+        // 'Tymon\JWTAuth\Providers\LaravelServiceProvider'
+
+        // /*
+        // * Application Service Providers...
+        // */  
 
     ],
 
@@ -226,7 +235,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
+        'JWTFactory' => 'Tymon\JWTAuth\Facades\JWTFactory'
     ],
 
 ];
